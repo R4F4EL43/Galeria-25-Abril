8d71d3f2d7fd99d649e10f496190205b9ca63f33
Rafael Brunheta <rafael.brunheta@hotmail.com>
2024-01-29 21:42:47 +0000

    Ja da para fazer upload de artes com imagens

---
+++ 25-Abril/ViewModels/ArteTipoArte_ViewModel.cs
@@ -0,0 +1,16 @@
+﻿using _25_Abril.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25_Abril.ViewModels
+{
+    public class ArteTiposArte_ViewModel
+    {
+        public Arte Arte { get; set; }
+        public IEnumerable<string> Tipos { get; set; }
+        public Tipo_de_Arte Tipo { get; set; }
+    }
+}
